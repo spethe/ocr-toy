7e53b283a1bf1618b9af249f1cde7ae6638cd06f
shubhajit.saha2 <shubhajit.saha2@gmail.com>
2013-02-26 10:30:08 +0000

    some basic routine added

--- CaptureAndRecognize/CaptureAndRecognize/CaptureAndRecognize.cpp
+++ CaptureAndRecognize/CaptureAndRecognize/CaptureAndRecognize.cpp
@@ -9,6 +9,8 @@ _fnRecognizeChars pFnRecognizeChars;
 _fnFreeEngine pFnFreeEngine;
 _fnEngineVersionInfo pFnEngineVersionInfo;
 
+int doSomething();
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	TCHAR strEnginesFilePath[MAX_FILE_PATH];
@@ -28,9 +30,92 @@ int _tmain(int argc, _TCHAR* argv[])
 	pFnFreeEngine        = (_fnFreeEngine)GetProcAddress(m_HIDevRecEngineDLL,"fn_FreeEngine");
 	pFnEngineVersionInfo = (_fnEngineVersionInfo)GetProcAddress(m_HIDevRecEngineDLL,"fn_EngineVersionInfo");
 
+	doSomething();
+
 	FreeLibrary(m_HIDevRecEngineDLL);
 	m_HIDevRecEngineDLL = NULL;
 
 	return 0;
 }
 
+int doSomething()
+{
+	//----Calibrate the camera / screen
+	//This image needs to be shown on the LCD screen
+	IplImage *image = cvLoadImage("fourCorners.bmp");
+	cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
+	cvShowImage("mywindow",image);
+
+	CvCapture* capture = cvCaptureFromCAM(0);
+	if ( !capture )
+	{
+		fprintf( stderr, "ERROR: capture is NULL \n" );
+		getchar();
+		return -1;
+	}
+
+	IplImage *curr_frame = cvQueryFrame(capture);
+
+	//--Calibrate
+	g_Resolution screenResolution;
+	screenResolution.width = 1024; //width of the screen
+	screenResolution.height = 600; //height of the screen
+	g_Resolution cameraResolution;
+	cameraResolution.width  = curr_frame->width;  //width of the camera
+	cameraResolution.height = curr_frame->height; //height of the camera
+
+	int calibrationFlag = pFnCaliberateCamera(curr_frame, cameraResolution, screenResolution);
+	if(calibrationFlag == 1)
+		printf("caliberation done\n");
+	else
+	{
+		printf("caliberation failed\n");
+		return 0;
+	}
+
+	//--Init engine
+	pFnInitEngine();
+	printf("Engine initialised successfully...\n");
+
+	int recog=0;
+	g_OCRCharInfo *g_Output=(g_OCRCharInfo *)malloc(sizeof(g_OCRCharInfo));
+	if(g_Output==NULL)
+		printf("Mem allocation problem");
+
+	while (true)
+	{
+		// Get one frame
+		curr_frame = cvQueryFrame( capture );
+		if ( !curr_frame )
+		{
+			fprintf( stderr, "ERROR: frame is null...\n" );
+			getchar();
+			break;
+		}
+		cvShowImage( "mywindow", curr_frame );
+
+		Sleep(10); // in milisecond
+
+		recog = pFnRecognizeChars(curr_frame,0,&g_Output);
+		printf("\n\n\nTest:chars recog count %d",recog);
+
+		for(int i=0; i<recog; i++)
+			printf("\nTest: Angle:%f Char:%x Bounded by:(%d,%d)(%d,%d) with centroid at(%d,%d) ",
+			g_Output->g_Angle[i],
+			g_Output->g_UnicodeChars[i],
+			(g_Output->boundingBoxes[i]).minX,
+			(g_Output->boundingBoxes[i]).minY,
+			(g_Output->boundingBoxes[i]).maxX,
+			(g_Output->boundingBoxes[i]).maxY,
+			(g_Output->centroids[i]).x,
+			(g_Output->centroids[i]).y);
+
+		if ((cvWaitKey(10) & 255) == 27) break;
+	}
+
+	cvDestroyWindow( "mywindow" );
+	cvReleaseCapture( &capture );
+
+	pFnFreeEngine();
+	printf("Engine freed successfully...\n");
+}
